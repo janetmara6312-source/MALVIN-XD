ce376f43822de38fb082e3e53ebcfcd85530e8b3
janetmara6312-source <janetmara6312@gmail.com>
2026-04-18 22:51:58 +0100

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,9 +1,9 @@
 //=====🏅 ᴍᴀʟᴠɪɴ  xᴅ🔥====
 
 module.exports = {
-  BOT_NAME: 'ᴍᴀʟᴠɪɴ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263714757857', // add your phone number
+  BOT_NAME: 'Ade ola ', // ur bot name
+  OWNER_NAME: 'Ade ola', //your name
+  OWNER_NUMBER: '2348144389985', // add your phone number
   SESSION_ID: '', // add your session ID
   MODE: "public", // public or private
   TIMEZONE: 'Africa/Harare' // ur timezone
